7a57cc78fb954a7d214de60b5c6c31558988539b
Joseph___ <118039462+josephethiopia@users.noreply.github.com>
2024-11-17 11:51:08 +0300

    Add content/data.ts

---
+++ content/data.ts
@@ -0,0 +1,92 @@
+
+export const content = {
+  "home": {
+    "name": "Yoseph Ashenafi",
+    "description": "Passionated Fullstack Developer",
+    "location": "Jimma",
+    "availability": "Available",
+    "image": {
+      "src": "",
+      "alt": "Yoseph Ashenafi"
+    },
+    "socialLinks": {
+      "github": "",
+      "twitter": "",
+      "dribbble": ""
+    }
+  },
+  "aboutMe": {
+    "image": {
+      "src": "",
+      "alt": "Yoseph Ashenafi"
+    },
+    "aboutMe": "Hey, I'm Yoseph, a passionate fullstack developer. I love solving problems and doing stuff with programming.  Over 3 years of experience in the field.",
+    "socialLinks": {
+      "twitter": "https://telegram.com/@joaccount",
+      "github": ""
+    },
+    "quickFacts": {
+      "column1": [
+        "Fullstack Developer",
+        "3+ Years Experience"
+      ],
+      "column2": [
+        "Jimma, Ethiopia",
+        "joash.000111@gmail.com"
+      ]
+    },
+    "outro": ""
+  },
+  "skills": {
+    "skills": [
+      {
+        "name": "React",
+        "icon": ""
+      },
+      {
+        "name": "Next.js",
+        "icon": ""
+      },
+      {
+        "name": "Zustand",
+        "icon": ""
+      },
+      {
+        "name": "AI",
+        "icon": ""
+      },
+      {
+        "name": "MongoDB",
+        "icon": ""
+      },
+      {
+        "name": "Express.js",
+        "icon": ""
+      },
+      {
+        "name": "Node.js",
+        "icon": ""
+      },
+      {
+        "name": "Tailwind CSS",
+        "icon": ""
+      },
+      {
+        "name": "shadcn UI",
+        "icon": ""
+      }
+    ]
+  },
+  "work": {
+    "projects": []
+  },
+  "contact": {
+    "email": "joash.000111@gmail.com",
+    "phone": "0941889660",
+    "header": "Get in Touch",
+    "socialLinks": {
+      "facebook": "https://fb.com/@joe",
+      "telegram": "https://telegram.com/@joaccount"
+    }
+  }
+}
